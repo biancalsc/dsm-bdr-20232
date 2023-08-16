64a03fa6579444d3a7e0a119b2d74cd8ea3f64cc
Bianca Lucas <110243524+biancalsc@users.noreply.github.com>
2023-08-15 22:59:58 -0300

    criação 2 tabelas

---
+++ aula03.sql
@@ -0,0 +1,15 @@
+create database bd_aula03;
+
+create table tbl_cliente (
+	codigo_cliente integer primary key,
+	nome varchar(50) not null,
+	cidade varchar(50),
+	endereco varchar (50)
+);
+
+create table tbl_titulo (
+	codigo_titulo integer primary key,
+	titulo varchar (10) not null,
+	descricao varchar (20),
+	categoria varchar (10)
+);
