f9018cd853a9758a3af56e8dd97cf3c4bb4b3456
Bianca Lucas <110243524+biancalsc@users.noreply.github.com>
2023-08-19 22:27:33 -0300

    adicionei as chaves estrangeiras

--- aula03.sql
+++ aula03.sql
@@ -8,32 +8,26 @@ create table tbl_cliente (
 	codigo_cliente integer primary key,
 	nome varchar(50) not null,
 	cidade varchar(50),
-	endereco varchar (50)
+	endereco varchar(50)
 );
+create domain chk_categoria varchar(7) check (value='drama' or value='comedia');
 
 create table tbl_titulo (
 	codigo_titulo integer primary key,
-	titulo varchar (10) not null,
-	descricao varchar (20),
-	categoria varchar (10)
+	titulo varchar(10) not null,
+	descricao varchar(20),
+	categoria chk_categoria
 );
+create table tbl_emprestimo (
+	numero_emprestimo integer primary key
+);
+create domain chk_status varchar(11) check (value='disponivel' or value='alugado');
 
 create table tbl_livros (
 	cod_livro integer primary key,
-	codigo_titulo integer,
-	status varchar (10)
+	status chk_status
 );
 
-create table tbl_emprestimo (
-	numero_emprestimo integer primary key,
-	codigo_cliente integer,
-	codigo_livro integer
-);
-
-CREATE DOMAIN chk_status text check (value='disponivel' OR value='alugado');
-
-CREATE DOMAIN chk_categoria text check (value='drama' OR value='comedia');
-
-ALTER TABLE tbl_titulo ALTER COLUMN categoria SET DATA TYPE chk_categoria;
-
-ALTER TABLE tbl_livros ALTER COLUMN status SET DATA TYPE chk_status;
+alter table tbl_emprestimo add column codigo_cliente integer references tbl_cliente (codigo_cliente);
+alter table tbl_emprestimo add column codigo_livro integer references tbl_livros (cod_livro);
+alter table tbl_livros add column codigo_titulo integer references tbl_titulo (codigo_titulo);
